f0a1e7f48d59c2dc531f66b734d4da4e44c1813d
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-08-22 21:42:08 +0100

    Images not displaying yet

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../'
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+    <header>
+        <nav className='flex'>
+
+        </nav>
+    </header>
+  )
+}
