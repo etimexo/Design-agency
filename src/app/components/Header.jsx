9bc395f9402df1115a8b8f94e54be8c8f9faa908
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-08-27 00:31:03 +0100

    Editting

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -9,7 +9,7 @@ import Arrow from "../arrowdown.png";
 
 export default function Header() {
   return (
-    <header id="#header" className="p-5">
+    <header id="#header" className="change p-5">
       <nav className="navbar shadow">
         <div className="logo rounded">
           <a href="#" className="border-none">
@@ -19,42 +19,48 @@ export default function Header() {
         <div className=" toggler">
           <Image className="toggleimg" src={Toggleimg} alt="Toggler icon" />
         </div>
-          <ul className="navmenu md:flex">
-            <li className="text-black pr-10 font-bold">
-              <a className="text-center border-none" href="#">
-                About
-              </a>
-            </li>
-            <li className="text-black md:pr-10 font-bold">
-              <a className="text-center border-none" href="#services">
-                Services
-              </a>
-            </li>
-            <li className="text-black pr-10 font-bold">
-              <a className="text-center border-none" href="#">
-                Contact
-              </a>
-            </li>
-          </ul>
+        <ul className="navmenu md:flex">
+          <li className="text-black pr-10 font-bold">
+            <a className="text-center border-none" href="#">
+              About
+            </a>
+          </li>
+          <li className="text-black md:pr-10 font-bold">
+            <a className="text-center border-none" href="#services">
+              Services
+            </a>
+          </li>
+          <li className="text-black pr-10 font-bold">
+            <a className="text-center border-none" href="#">
+              Contact
+            </a>
+          </li>
+        </ul>
       </nav>
       <div className="arrowdown">
         <Image className="arrowd arra" src={Arrow} alt="arrow-down icon" />
       </div>
       {/* <Image src={Bgmobile} alt="Background image for mobile screen" /> */}
-      
+
       <section className="sec1 text-left font-size mt-[8vh] ">
-        <h1 className=" mt-5 py-5 text-blue-700">
-          Enhance a healthier you: Overcome your fears with our support!
+        <h1 className=" py-5 text-white">
+          Unlock Your Potential, Elevate Your Fitness!
         </h1>
-        <p className="text-slate-800">
-          Welcome to our empowering fitness community, where we believe that
-          true transformation happens when you step outside your comfort zone.
-          Are your fears holding you back from achieving your fitness goals? It
-          is time to break free from those limitations and embark on a journey
-          of self-discovery and growth.
+        <p className="text-white">
+          Elevate your fitness and unveil a new you! Join us in embracing a
+          transformative journey that ignites potential and surpasses limits.
+          Let&apos;s sculpt a stronger, healthier version of yourself together.
+          Explore more to start your transformation
         </p>
+        <a href="#services">
+          {" "}
+          <button className="mt-[5vh] btn btn1">Learn more</button>
+        </a>
+        <a href="#signup">
+          {" "}
+          <button className="btn btn2">Get started</button>
+        </a>
       </section>
-      
     </header>
   );
 }
