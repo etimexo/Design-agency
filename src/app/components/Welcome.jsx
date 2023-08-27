84c2c783f51829e16c3c8e897e40b708ffb6c6b2
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-08-27 20:13:43 +0100

    Create new component

---
+++ src/app/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'next/image';
+import './Welcome.css';
+import Wel from '../welcome.jpg';
+
+export default function Welcome() {
+  return (
+    <section>
+
+    </section>
+  )
+}
