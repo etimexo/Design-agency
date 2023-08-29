4d052fcfbbc684b49f6053e1faa6515665e455ae
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-08-29 21:55:59 +0100

    Style Welcome.jsx component

--- src/app/components/Welcome.jsx
+++ src/app/components/Welcome.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 import "../components/Welcome.css";
 
 export default function Welcome() {
   return (
     <div>
-      
+      <nav>
+        <div className="d">
+          <a href="#" className="blog-link nav-link">
+            Blog
+          </a>
+          <a href="#" className="contact-link nav-link">
+            Contact
+          </a>
+        </div>
+
+        <a href="#">
+          <div className="logo">Gyme</div>
+        </a>
+
+        <div className="toggler">
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+
+        {/* <a className="desktop" href="#">
+          <button className="sign-btn">SIGN UP</button>
+        </a> */}
+
+        {/* <ul className="navmenu">
+          <a href="#">
+            <button className="sign-btn">SIGN UP</button>
+          </a>
+        </ul> */}
+
+        
+      </nav>
     </div>
-  )
+  );
 }
