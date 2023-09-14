8c37c105900ba96aefb437a4cda280fc0943cea8
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-09-14 22:22:44 +0100

    Edit files

---
+++ src/app/components/Other.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "../components/Other.css";
+import Stretch from '../stretch.jpg';
+import Image from "next/image";
+
+export default function Other() {
+  return (
+    <section className="sec3">
+      <div className="other">
+      <p className="other-p">
+        “I learned so much from Shayna. My excuses were stopping me from
+        sticking to a healthy eating plan and workout schedule. I&apos;ve been
+        consistent with my new regimen ever since.”
+      </p>
+      <p>Gymian G.</p> <br /> <br />
+      </div>
+      <div className="vary">
+        <Image src={Stretch} className="stretch" alt="woman stretching" />
+        <div className="vary2">
+            <p className="h">Warm Up</p>
+            Work Out
+            Cool Down
+
+            <p>
+              Proper stretching and cool down routines will
+              help you maximize your workouts ang get 
+              noticeable results fast.
+            </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Welcome from "./components/Welcome";
 import Services from "./components/Services";
-import './components/Welcome.css';
+import Other from "./components/Other";
 
 export default function page() {
   return (
     <body>
       <Welcome />
       <Services />
+      <Other />
     </body>
   )
 }
