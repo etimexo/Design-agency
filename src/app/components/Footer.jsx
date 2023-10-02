73fe67e431aa3ffcb9166671d6d12a768a7e76b3
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-10-02 23:56:08 +0100

    Style Footer component

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import "../components/Footer.css";
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <footer>
+      <p className="name">Gymian G.</p>
+      <p className="body">
+        Personal Fitness Trainer + Nutritionist based in Los Angeles,
+        Carlifornia.
+      </p>
+      <p className="under"><u><a href="https://www.squarespace.com/" target="_blank">Design by SquareSpace</a></u></p>
+      <p className="under"><u><a href="https://gentledove.netlify.app" target="_blank">Cloned by GentleDove</a></u></p>
+    </footer>
+  );
 }
