0cbf41013deace6c7f39e415dd65f2ae6668b2de
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-10-03 23:11:38 +0100

    Style footer component

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,25 +4,41 @@ import "../components/Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <p className="name">Gymian G.</p>
-      <p className="body">
-        Personal Fitness Trainer + Nutritionist based in Los Angeles,
-        Carlifornia.
-      </p>
-      <p className="under">
-        <u>
-          <a href="https://www.squarespace.com/" target="_blank">
-            Design by SquareSpace
-          </a>
-        </u>
-      </p>
-      <p className="under">
-        <u>
-          <a href="https://gentledove.netlify.app" target="_blank">
-            Cloned by GentleDove
-          </a>
-        </u>
-      </p>
+      <div className="first">
+        <p className="name">Gymian G.</p>
+      </div>
+      <div className="second">
+        <p className="body">
+          Personal Fitness Trainer + Nutritionist based in Los Angeles,
+          Carlifornia.
+        </p>
+        <p className="under">
+          <u>
+            <a href="https://www.squarespace.com/" target="_blank">
+              Design by SquareSpace
+            </a>
+          </u>
+        </p>
+        <p className="under">
+          <u>
+            <a href="https://gentledove.netlify.app" target="_blank">
+              Cloned by GentleDove
+            </a>
+          </u>
+        </p>
+      </div>
+      <div className="touch">
+        <p className="body">Get in touch</p>
+        <a href="mailto:elijahobisesan01@gmail.com" className="l">
+          <u>Email</u>
+        </a><br />
+        <a href="#" className="l">
+          <u>Signup</u>
+        </a><br />
+        <a href="#" className="l">
+          <u>Newsletter</u>
+        </a>
+      </div>
     </footer>
   );
 }
