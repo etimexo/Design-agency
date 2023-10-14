77d571e5e2b635fc3d8033ea960bdd93057c1461
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-10-14 10:36:54 +0100

    Fix link

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import Header from '../components/Header';
+import React from "react";
+import Image from "next/image";
+import Header from "../components/Header";
+import "../signup.css";
+import S1 from "../s1.jpg";
 
 export default function page() {
   return (
-    <section className='sign-up'>
+    <section className="sign-up">
       <Header />
       <h1>GET BACK ON TRACK</h1>
+      <Image src={S1} alt="a female trainee" className="s1" />
+      <h1>
+        By applying my methods and setting your goals, you&apos;ll realize how
+        manageable and natural each step becomes.
+      </h1>
+      <a className="desktop" href="#">
+        <button className="sign-btn">SCHEDULE YOUR FIRST SESSION</button>
+      </a>
     </section>
-  )
+  );
 }
