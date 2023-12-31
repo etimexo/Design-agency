1729e80698f3af3d0f3a1df352b6b352c84a771f
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-12-31 18:08:49 +0100

    Styled booking1.jsx component

--- src/app/components/Booking1.jsx
+++ src/app/components/Booking1.jsx
@@ -1,18 +1,28 @@
 "use client";
-import React from "react";
-import { useState } from "react";
+// import React from "react";
+// import { useState } from "react";
+// import useRef from "react"
+import React, { useState, useRef } from "react";
 import "../components/Booking1.css";
 import Booking2 from "./Booking2";
-import { stringify } from "postcss";
 export default function Booking1() {
   const [initial, first] = useState(false);
+  const [hrInputValue, setHrInputValue] = useState("")
+  const hrInputRef = useRef(null);
+
   function onClick() {
     first(true);
   }
-  
-  let hrText = document.getElementsByClassName("inp")
+
+  let hrText = document.getElementById("hr-inp");
+  let phr = document.getElementById("phr")
+  // function hrclicked() {
+  //   alert(hrText.value)
+  // }
   function hrclicked() {
-    alert(stringify(hrText.value))
+    // alert(hrInputRef.current.value);
+    // phr.innerText = hrInputRef.current.value
+    // alert(setHrInputValue)
   }
 
   return (
@@ -22,18 +32,25 @@ export default function Booking1() {
           <p className="h">Select Appointment</p>
           <div className="box1">
             <div className="sec1">
-              Example Service <br />
+              <span id="phr" ref={phr}>
+                Example Service
+              </span>
+              <br />
               1 hour <br />
               <input
                 type="text"
                 placeholder="Description of your service"
                 className="inp"
-                // value={hr-value}
-              /> <button className="ok-hr" onClick={hrclicked}>OK</button>
+                id="hr-inp"
+                ref={hrInputRef}
+                value={hrInputValue}
+              />{" "}
+              <button className="ok-hr" onClick={hrclicked}>
+                OK
+              </button>
             </div>
             <div className="sec2">
-            <button className="book-hr">Book</button>
-
+              <button className="book-hr">Book</button>
             </div>
           </div>
           <div className="box2">
@@ -44,10 +61,12 @@ export default function Booking1() {
                 type="text"
                 placeholder="Description of your service"
                 className="inp"
-              /> <button className="ok-min">OK</button>
+                id="min-inp"
+              />{" "}
+              <button className="ok-min">OK</button>
             </div>
             <div className="sec2">
-            <button className="book-min">Book</button>
+              <button className="book-min">Book</button>
             </div>
           </div>
           <button id="button" onClick={onClick}>
