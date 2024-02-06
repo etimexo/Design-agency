2818cf15cecee5ef9a4c442295c08f6ad13718ce
Elijah Obisesan <elijahobisesan01@gmail.com>
2024-02-06 22:00:13 +0100

    Remove Booking21 and Booking22 components and their associated CSS files.

--- src/app/components/Booking1.jsx
+++ src/app/components/Booking1.jsx
@@ -5,17 +5,11 @@ import "../components/Booking1.css";
 import Booking21 from "./Booking21";
 export default function Booking1() {
   // const [initial, first] = useState(false);
-  const [firstClick, setFirstClick] = "false"
+  const [firstClick, setFirstClick] = "false";
   const [hrInputValue, setHrInputValue] = useState("");
   const [minInputValue, setMinInputValue] = useState("");
   const hrInputRef = useRef(null);
   const minInputRef = useRef(null);
-
-
-  // function onClick() {
-    // first(true)
-  // }
-
   let hrText = document.getElementById("hr-inp");
   let phr = document.getElementById("phr");
 
@@ -26,62 +20,78 @@ export default function Booking1() {
   function handleInputChange2(event) {
     setMinInputValue(event.target.value);
   }
+  function bookHrClicked() {
+    if (!hrInputValue) {
+      alert ("Please input a valid description of your session")
+    } else {
+      alert(
+        "Your request has been submitted successfully. Thanks for booking a session with us"
+      );
+      setHrInputValue("");
+    }
+    
+  }
+  function bookMinClicked() {
+    if (!minInputValue) {
+      alert ("Please input a valid description of your session")
+    }
+    alert(
+      "Your request has been submitted successfully. Thanks for booking a session with us"
+    )
+    setMinInputValue("")
+  }
 
   return (
     <>
-      {/* {!initial && ( */}
-        <div className="booking1">
-          <p className="h">Select Appointment</p>
-          <div className="box1">
-            <div className="sec1">
-              <span id="phr">
-                {hrInputValue ? hrInputValue : "Example Service"}
-              </span>
-              <br />
-              1 hour <br />
-              <input
-                type="text"
-                placeholder="Description of your service"
-                className="inp"
-                id="hr-inp"
-                ref={hrInputRef}
-                value={hrInputValue}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="sec2">
-              <button className="book-hr">Book</button>
-            </div>
+      <div className="booking1">
+        <p className="h">Select Appointment</p>
+        <div className="box1">
+          <div className="sec1">
+            <span id="phr">
+              {hrInputValue ? hrInputValue : "Example Service"}
+            </span>
+            <br />
+            1 hour <br />
+            <input
+              type="text"
+              placeholder="Description of your service"
+              className="inp"
+              id="hr-inp"
+              ref={hrInputRef}
+              value={hrInputValue}
+              onChange={handleInputChange}
+            />
           </div>
-          <div className="box2">
-            <div className="sec1">
-              <span id="pmin">
-                {minInputValue ? minInputValue : "Example Service"}
-              </span>{" "}
-              <br />
-              30 minutes <br />
-              <input
-                type="text"
-                placeholder="Description of your service"
-                className="inp"
-                id="min-inp"
-                ref={minInputRef}
-                value={minInputValue}
-                onChange={handleInputChange2}
-              />
-            </div>
-            <div className="sec2">
-              <button className="book-min">Book</button>
-            </div>
+          <div className="sec2">
+            <button className="book-hr" onClick={bookHrClicked}>
+              Book
+            </button>
           </div>
-          {/* <button id="button" onClick={onClick}>
-            Click
-          </button> */}
-          <button onClick={() => setFirstClick(!firstClick)}>click</button>
-          <Booking21 />
         </div>
-      {/* )} */}
-      {/* {initial && <Booking21 />} */}
+        <div className="box2">
+          <div className="sec1">
+            <span id="pmin">
+              {minInputValue ? minInputValue : "Example Service"}
+            </span>
+            <br />
+            30 minutes <br />
+            <input
+              type="text"
+              placeholder="Description of your service"
+              className="inp"
+              id="min-inp"
+              ref={minInputRef}
+              value={minInputValue}
+              onChange={handleInputChange2}
+            />
+          </div>
+          <div className="sec2">
+            <button className="book-min" onClick={bookMinClicked}>
+              Book
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/app/components/Booking22.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Booking22() {
-  return (
-    <div>Booking22</div>
-  )
-}
